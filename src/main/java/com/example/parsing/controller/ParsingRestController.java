3eb736aa6eda2c9cf26e1a80b2cf030380e86c6c
taesan <pawer02023@gmail.com>
2022-03-08 17:34:39 +0900

    feat: add validation check
    - FormData 유효성 검사로직 추가
    - validation을 위한 library추가 (validation-starter)

--- src/main/java/com/example/parsing/controller/ParsingRestController.java
+++ src/main/java/com/example/parsing/controller/ParsingRestController.java
@@ -1,69 +1,26 @@
 package com.example.parsing.controller;
 
 import com.example.parsing.domain.FormData;
-import com.example.parsing.domain.ParsingDto;
-import lombok.AllArgsConstructor;
+import com.example.parsing.service.ParsingService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.LaxRedirectStrategy;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.ResourceAccessException;
-import org.springframework.web.client.RestTemplate;
 
-import static com.example.parsing.utils.ApiUtils.error;
+import javax.validation.Valid;
+
 import static com.example.parsing.utils.ApiUtils.success;
 
 @Slf4j
-@RestController @AllArgsConstructor
+@RestController @RequiredArgsConstructor
 public class ParsingRestController {
 
-    private String getData(String url) {
-        RestTemplate restTemplate = new RestTemplate();
-        HttpComponentsClientHttpRequestFactory factory =
-                new HttpComponentsClientHttpRequestFactory();
-
-        HttpClient httpClient =
-                HttpClientBuilder.create()
-                        .setRedirectStrategy(new LaxRedirectStrategy())
-                        .build();
-
-        factory.setHttpClient(httpClient);
-        restTemplate.setRequestFactory(factory);
-
-        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-        return response.getBody();
-    }
-
-    private ParsingDto getParsingDto(int unit, String data) {
-        // make data
-        int nmg = data.length() / unit;
-        int substrIdx = data.length() - nmg;
-
-        String content = data.substring(0, substrIdx);
-        String remain = data.substring(substrIdx);
-
-        ParsingDto parsingDto = ParsingDto.builder()
-                .content(content)
-                .remain(remain)
-                .build();
-        return parsingDto;
-    }
+    private final ParsingService parsingService;
 
     @PostMapping("/parsing")
-    public ResponseEntity parsing_api(FormData formData) {
+    public ResponseEntity parsing_api(@Valid FormData formData) {
         log.info(formData.toString());
-        // call
-        try {
-            String data = getData(formData.getUrl());
-            ParsingDto parsingDto = getParsingDto(formData.getUnit(), data);
-            return ResponseEntity.ok(success(parsingDto));
-        } catch(ResourceAccessException e) {
-            return ResponseEntity.badRequest().body(error("올바르지 않은 URL", HttpStatus.BAD_REQUEST));
-        }
+        return ResponseEntity.ok(success(parsingService.getUrlData(formData)));
     }
 }
--- src/main/java/com/example/parsing/domain/FormData.java
+++ src/main/java/com/example/parsing/domain/FormData.java
@@ -1,11 +1,17 @@
 package com.example.parsing.domain;
 
 import lombok.*;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotBlank;
 
 @Getter @Setter @Builder @ToString
 @NoArgsConstructor @AllArgsConstructor
 public class FormData {
+    @NotBlank(message = "Url must be exist")
     private String url;
+    @Range(min = 1, max = 2, message = "Type 1(remove tag) or 2(all) only")
     private int type;
+    @Range(min = 1l, message = "Unit Please a number greater than 1")
     private int unit;
 }

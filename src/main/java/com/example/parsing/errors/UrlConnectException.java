7c3b44f1c5c7fb7224d01f015ad1fa33e453be47
taesan <pawer02023@gmail.com>
2022-03-11 20:17:15 +0900

    feat: create RuntimeException
    - Jsoup을 통해 URL 요청시, 에러나면 RuntimeException을 생성하여 ExcpetionHandler에서 처리하기
    위함.

---
+++ src/main/java/com/example/parsing/errors/UrlConnectException.java
@@ -0,0 +1,7 @@
+package com.example.parsing.errors;
+
+public class UrlConnectException extends RuntimeException {
+    public UrlConnectException(String message) {
+        super(message);
+    }
+}

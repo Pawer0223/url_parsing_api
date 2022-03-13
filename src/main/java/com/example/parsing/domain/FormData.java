ec67cc3b01eb2c37ec837be618dc56de5170841e
taesan <pawer02023@gmail.com>
2022-03-13 14:24:38 +0900

    refactor: modify type
    primitive type -> wrapper class change

--- src/main/java/com/example/parsing/domain/FormData.java
+++ src/main/java/com/example/parsing/domain/FormData.java
@@ -12,7 +12,7 @@ public class FormData {
     @NotBlank(message = "Url must be exist")
     private String url;
     @Range(min = 1, max = 2, message = "Type 1(remove tag) or 2(all) only")
-    private int type;
+    private Integer type;
     @Range(min = 1, max = 10000000, message = "Unit Please a number greater than 1 and less than 1000000")
-    private int unit;
+    private Integer unit;
 }
